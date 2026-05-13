85e5f6d8cbc9d6280088cbc90a4b553f386ad071
Fabrizio640 <fabriziomatias52@gmail.com>
2026-05-12 22:54:31 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from "react";
+import { useNavigate } from 'react-router-dom'
 import "./App.css";
 
 import logo from "./assets/img/Logonetflix.png";
 import catalogoImg from "./assets/img/netflix.png";
 
 export default function App() {
-  const [login, setLogin] = useState("");
-  const [senha, setSenha] = useState("");
+  const navigate = useNavigate()
 
   return (
     <main>
@@ -23,21 +22,28 @@ export default function App() {
         <input
           type="text"
           placeholder="Digite seu login..."
-          value={login}
-          onChange={(e) => setLogin(e.target.value)}
         />
 
         <label className="label">Senha</label>
         <input
           type="password"
           placeholder="Digite sua senha..."
-          value={senha}
-          onChange={(e) => setSenha(e.target.value)}
         />
 
-        <button className="btn-entrar">ENTRAR</button>
+        <button className="btn-entrar" onClick={() => navigate('/listagem')}>
+          ENTRAR
+        </button>
 
-        <a href="#" className="link-forgot">Esqueci meu login ou senha</a>
+        <a href="#" className="link-forgot">
+          Esqueci meu login ou senha
+        </a>
+
+        <p className="cadastro-texto">
+          Não possui conta?
+          <a href="#" className="link-cadastro" onClick={(e) => { e.preventDefault(); navigate('/cadastro') }}>
+            Faça o cadastro
+          </a>
+        </p>
       </div>
     </main>
   );
